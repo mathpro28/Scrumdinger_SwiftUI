5c15745f471ca7ba1a0d4cd7c6959b9b0844b832
mathpro28 <matemerc@gmail.com>
2024-01-24 10:15:33 -0400

    Added view that displays the history of a meeting

---
+++ Models/History.swift
@@ -0,0 +1,22 @@
+//
+//  History.swift
+//  Scrumdinger
+//
+//  Created by Mateo Mercado Maguiña on 16/1/24.
+//
+
+import Foundation
+
+struct History: Identifiable, Codable {
+    let id: UUID
+    let date: Date
+    var attendees: [DailyScrum.Attendee]
+    var transcript: String?
+    
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee], transcript: String? = nil) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+        self.transcript = transcript
+    }
+}
