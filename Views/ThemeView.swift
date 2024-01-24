233d8e816b50e9f2f061706d29111aa380360165
mathpro28 <matemerc@gmail.com>
2024-01-24 19:10:45 -0400

    Added file to organise themes for the project.

---
+++ Views/ThemeView.swift
@@ -0,0 +1,27 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by Mateo Mercado Maguiña on 15/1/24.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    let theme: Theme
+    
+    var body: some View {
+        Text(theme.name)
+            .padding(4)
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            .background(theme.mainColor)
+            .foregroundColor(theme.accentColor)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+}
+
+struct ThemeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemeView(theme: .buttercup)
+    }
+}
