c69010f07dbe1d80cebff48e3b576c771a5cda32
mathpro28 <matemerc@gmail.com>
2024-01-24 22:24:21 -0400

    Added the main view which calls to the other views and modules.

---
+++ Views/ScrumsView.swift
@@ -0,0 +1,51 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Mateo Esteban Mercado Maguiña on 18/12/23.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    @Binding var scrums: [DailyScrum]
+    @Environment(\.scenePhase) private var scenePhase
+    @State private var isPresentingNewScrumView = false
+    let saveAction: ()->Void
+    
+    var body: some View {
+        NavigationStack {
+            List($scrums) { $scrum in
+                NavigationLink(destination: DetailView(scrum: $scrum)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {
+                    isPresentingNewScrumView = true
+                }) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
+            }
+        }
+        .sheet(isPresented: $isPresentingNewScrumView) {
+            NewScrumSheet(scrums: $scrums, isPresentingNewScrumView: $isPresentingNewScrumView)
+        }
+        .onChange(of: scenePhase) { newScenePhase, oldScenePhase in
+            if newScenePhase == .inactive {
+                saveAction()
+            }
+        }
+
+
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: .constant(DailyScrum.sampleData), saveAction: {})
+    }
+}
