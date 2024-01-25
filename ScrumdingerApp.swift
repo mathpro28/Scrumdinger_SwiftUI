411b81beeb437d2f242c5fb0b4d532a268dd8621
mathpro28 <matemerc@gmail.com>
2024-01-24 20:59:02 -0400

    Added file that is in charge of calling the other views and initialising everything.

---
+++ ScrumdingerApp.swift
@@ -0,0 +1,40 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Mateo Esteban Mercado Maguiña on 14/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    @StateObject private var store = ScrumStore()
+    @State private var errorWrapper: ErrorWrapper?
+    
+    var body: some Scene {
+        WindowGroup {
+            ScrumsView(scrums: $store.scrums) {
+                Task {
+                    do {
+                        try await store.save(scrums: store.scrums)
+                    } catch {
+                        errorWrapper = ErrorWrapper(error: error, guidance: "Try again later.")
+                    }
+                }
+            }
+            .task {
+                do {
+                    try await store.load()
+                } catch {
+                    errorWrapper = ErrorWrapper(error: error, guidance: "Scrumdinger will load sample data and continue.")
+                }
+            }
+            .sheet(item: $errorWrapper) {
+                store.scrums = DailyScrum.sampleData
+            } content: { wrapper in
+                ErrorView(errorWrapper: wrapper)
+            }
+        }
+    }
+}
